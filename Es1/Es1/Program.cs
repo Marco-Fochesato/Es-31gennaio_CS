dbac7fcbec4720d955932c1be92f043eb25e14d8
Marco-Fochesato <14529834@itisrossi.vi.it>
2026-02-01 10:12:05 +0100

    primo commit

---
+++ Es1/Es1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Es1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci codice di verifica:");
+            bool x= int.TryParse(Console.ReadLine(), out int codice);
+
+            int originalCodice = codice;
+            int temp = 0;
+            int somma = 0;
+            int numCifre = codice.ToString().Length;
+    
+            while (codice > 0)
+            {
+                temp= codice % 10;
+                codice = codice / 10;
+
+                somma += (int)Math.Pow(temp, numCifre);
+
+            }
+            if (somma == originalCodice)
+            {
+                Console.WriteLine("Il numero è un numero di Armstrong");
+            }
+            else
+            {
+                Console.WriteLine("Il numero non è un numero di Armstrong");
+            }
+        }
+    }
+}
